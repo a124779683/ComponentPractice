caa574d7d04c2819cc51caaa78cff86dba6f2f0f
a124779683 <215041631@qq.com>
2017-03-15 10:09:08 +0800

    添加home模块

---
+++ home/src/main/java/com/jhb/home/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.jhb.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author jianghongbo
+ * @version 1.0
+ * @file HomeActivity.java
+ * @brief
+ * @date 2017/3/15
+ * Copyright (c) 2017, 上品折扣
+ * All rights reserved.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity_home);
+    }
+}
